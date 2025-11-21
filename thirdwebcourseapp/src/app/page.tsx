a42f9f6efa8898301fa99805f862f4dd0128d028
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-21 23:21:20 +0000

    contract card for each contract

--- thirdwebcourseapp/src/app/page.tsx
+++ thirdwebcourseapp/src/app/page.tsx
@@ -1,6 +1,13 @@
-"use client";
-
+import ContractCard from "@/components/contract-card";
 import styles from "../styles/Home.module.css";
+import {
+  ERC1155_CONTRACT_ADDRESS,
+  ERC20_CONTRACT_ADDRESS,
+  ERC721_CONTRACT_ADDRESS,
+  PROFILE_STATUS_CONTRACT_ADDRESS,
+  STAKING_CONTRACT_ADDRESS,
+  TIP_JAR_CONTRACT_ADDRESS,
+} from "@/constants/addresses";
 
 export default function Home() {
   return (
@@ -14,6 +21,45 @@ export default function Home() {
           <p className={styles.description}>
             Select a contract to interact with.
           </p>
+        </div>
+
+        <div className={styles.grid}>
+          <ContractCard
+            href="/"
+            contractAddress={ERC20_CONTRACT_ADDRESS}
+            title="ERC20 ->"
+            description="Claim ERC20 Token"
+          />
+          <ContractCard
+            href="/"
+            contractAddress={ERC721_CONTRACT_ADDRESS}
+            title="ERC721 ->"
+            description="Claim ERC721 Token"
+          />
+          {/* <ContractCard
+            href="/"
+            contractAddress={ERC1155_CONTRACT_ADDRESS}
+            title="ERC1155 ->"
+            description="Claim ERC1155 Token"
+          /> */}
+          <ContractCard
+            href="/"
+            contractAddress={STAKING_CONTRACT_ADDRESS}
+            title="Staking ->"
+            description="Stake your ERC721 NFT to earn ERC20"
+          />
+          <ContractCard
+            href="/"
+            contractAddress={PROFILE_STATUS_CONTRACT_ADDRESS}
+            title="Profile Status ->"
+            description="Update your profile status on the blockchain"
+          />
+          <ContractCard
+            href="/"
+            contractAddress={TIP_JAR_CONTRACT_ADDRESS}
+            title="Tip Jar ->"
+            description="Leave a tip on the blockchain"
+          />
         </div>
       </div>
     </main>
