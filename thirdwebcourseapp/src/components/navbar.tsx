1f9763bb21b9b11ef2b0ee78da9c7f98b9f4acc4
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-21 19:46:55 +0000

    create navbar with wallet connect button

---
+++ thirdwebcourseapp/src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { ThirdwebProvider, ConnectButton } from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+
+const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const secretKey = process.env.NEXT_PUBLIC_TEMPLATE_SECRET_KEY;
+const client = createThirdwebClient({
+  clientId: clientId as string,
+  secretKey: secretKey as string,
+});
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Link href="/">
+        <p
+          className={styles.gradientText1}
+          style={{ cursor: "pointer", fontSize: "1.2rem", fontWeight: "bold" }}
+        >
+          Tiago Porfolio
+        </p>
+      </Link>
+
+      <ConnectButton client={client} />
+    </div>
+  );
+}
